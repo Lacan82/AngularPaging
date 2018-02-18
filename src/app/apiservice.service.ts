05f2bb89fe44a79398becefb99778eb8b97b2fd5
Lacan <dmorgan@digital-restart.com>
2018-02-18 11:56:38 -0500

    Added Constants
    Signed-off-by: Lacan <dmorgan@digital-restart.com>

--- src/app/apiservice.service.ts
+++ src/app/apiservice.service.ts
@@ -1,24 +1,29 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Sessions } from './sessions.model';
+import { ConstantsService } from './constants.service';
 import 'rxjs/add/operator/map';
+
 
 
 @Injectable()
 export class ApiserviceService {
+  cursor;
+  more: boolean;
 
-  constructor(private http: HttpClient) { }
-  GetArchivedSensordata() {
-    return this.http.get<Sessions[]>('https://sensordata-dot-hyperwave-168417.appspot.com/_ah/api/sessions/v1/read/')
+  constructor(private http: HttpClient,
+              private base_url: ConstantsService) { }
+  GetArchivedSensordata(cursor = '') {
+    return this.http.get<Sessions[]>( this.base_url.BaseUrl() + '/read?cursor=' + cursor)
       .map(
         (response) => {
-
-          return response;
+          const result = response['items'];
+          this.cursor = response['cursor']['cursor'];
+          this.more = response['more'];
+          console.log(result);
+          return result;
         }
 
       );
   }
-
-
-
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import { ApiserviceService } from './apiservice.service';
 import {Sessions} from './sessions.model';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/Rx';
+
 
 
 @Component({
@@ -8,19 +11,48 @@ import {Sessions} from './sessions.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  session: Sessions[];
+export class AppComponent implements OnInit, OnChanges {
+  session = [];
+
+  sessionCount = 0;
+  cursor = [];
+  more: boolean;
 
 
-  constructor(private apiservice: ApiserviceService){}
+
+  constructor(private apiservice: ApiserviceService) {
+  }
 
   ngOnInit() {
-    this.CollectArchive();
+      this.CollectArchive();
+    // const iterator = Observable.interval(1000);
+    // iterator.subscribe(() => {
+    //   if (this.more === true) { this.CollectArchive(); console.log('running');
+    //   this.sessionCount += this.session.length; } else { this.sessionCount += this.session.length; }
+    // }
   }
 
   CollectArchive() {
-    this.apiservice.GetArchivedSensordata().subscribe(data => this.session = (data));
+    this.apiservice.GetArchivedSensordata(this.cursor[this.cursor.length - 1]).subscribe(data => {this.session = this.session.concat(data);
+    this.cursor.push(this.apiservice.cursor);
+    this.more = this.apiservice.more;
+    });
 
-}}
+}
+  // ngDoCheck() {
+  //   if (this.more === true) {
+  //     this.apiservice.GetArchivedSensordata(this.cursor).subscribe(data => {this.session = (data);
+  //       this.cursor = data['cursor']['cursor'];
+  //       this.more = data['more'];
+  //       console.log(this.session);
+  //     } );
+  //   }
+  // }
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes);
+     }
 
+
+
+}
 
